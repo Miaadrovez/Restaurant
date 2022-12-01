65a62289268d469680b8c5e33d3f98c51fb85d46
Miaadrovez <madrovez@alumnosuls.cl>
2022-12-01 00:20:00 -0300

    0.0.1v

---
+++ src/proyectorestaurante/ProyectoRestaurante.java
@@ -0,0 +1,14 @@
+
+package proyectorestaurante;
+
+public class ProyectoRestaurante {
+
+    public static void main(String[] args) {
+        //MyFrame myFrame = new MyFrame();
+        Panel panelUno = new Panel();
+        
+        //Login login = new Login();
+        //PopUp panelControl = new PopUp();
+        //MyFrame.jButton1ActionPerformed= jButton1ActionPerformed ;
+    }
+}
